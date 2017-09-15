ad7c23912eb114e0d9a3f58b49701e7f4bdc9497
ferrata <al.goloshchapov@gmail.com>
2017-09-15 06:35:49 -0500

    find-the-running-median

--- ctci/find-the-running-median.cpp
+++ ctci/find-the-running-median.cpp
@@ -23,15 +23,38 @@
 
 using namespace std;
 
-
 int main(){
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    for ( int a_i = 0;a_i < n;a_i++ ) {
-        cin >> a[a_i];
-        
-        
+    int count;
+    cin >> count;
+    
+    double median = 0;
+    cout.precision( 1 );
+    
+    priority_queue<int, vector<int>, less<int> > left;
+    priority_queue<int, vector<int>, greater<int> > right;
+    
+    for ( int i = 0; i < count; i++ ) {
+        int v;
+        cin >> v;
+
+        if ( left.size() == right.size() )
+            left.push( v );
+        else 
+            right.push( v );
+
+        if ( right.size() && left.top() > right.top() ) {
+            left.push( right.top() );
+            right.push( left.top() );
+            left.pop();
+            right.pop();
+            }
+
+        if ( left.size() == right.size() )
+            median = ( left.top() + right.top() ) * .5;
+        else
+            median = left.top();
+            
+        cout << fixed << median << endl;
         }
     return 0;
     }
