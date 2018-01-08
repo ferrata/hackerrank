353763c4d5d03f1b2dcc6301abce4bf80564ca99
ferrata <al.goloshchapov@gmail.com>
2018-01-08 13:54:02 -0600

    progress

--- ctci/array-manipulation.cpp
+++ ctci/array-manipulation.cpp
@@ -11,19 +11,19 @@ int main() {
     int m;
     cin >> n >> m;
 
-    vector<int> arr;
-    for (size_t i=0; i<n; i++)
+    vector<long long> arr;
+    for (size_t i = 0; i < n; i++)
         arr.push_back(0);
 
-    for(int a0 = 0; a0 < m; a0++){
+    for (int a0 = 0; a0 < m; a0++) {
         int a; int b; int k;
         cin >> a >> b >> k;
-        
-        for (size_t i=a-1; i<b-1; i++)
+        for (size_t i = a-1; i < b; i++) {
             arr[i] += k;
+        }
     }
 
-    vector<int>::iterator found = max_element(arr.begin(), arr.end());
+    vector<long long>::iterator found = max_element(arr.begin(), arr.end());
     cout << *found;
 
     return 0;
