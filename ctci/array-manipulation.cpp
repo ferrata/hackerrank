929d46bb15a16732d9354751e269d08107bdb741
ferrata <al.goloshchapov@gmail.com>
2018-01-10 23:53:54 -0600

    solved

--- ctci/array-manipulation.cpp
+++ ctci/array-manipulation.cpp
@@ -6,25 +6,57 @@ https://www.hackerrank.com/challenges/crush/problem
 
 using namespace std;
 
+class ranges_t {
+    typedef pair<int, int> range_t;
+    typedef map<range_t, long long> ranges_map_t;
+
+    ranges_map_t ranges_;
+
+public:
+    void add(int from, int to, int value) {
+        auto item = ranges_.insert(make_pair(range_t(from, to), value));
+        if (item.second == false)
+            item.first->second += value;
+    }
+
+    long long find_max() {
+        long long max = 0;
+        long long current = 0;
+        map<int, long long> window;
+
+        for (const auto& it: ranges_) {
+            auto range = it.first;
+            auto range_value = it.second;
+            
+            auto remove_up_to = window.lower_bound(range.first);
+            for (auto r = window.begin(); r != remove_up_to; r++)
+                current -= r->second;
+            window.erase(window.begin(), remove_up_to);
+
+            auto item = window.insert(make_pair(range.second, range_value));
+            if (item.second == false)
+                item.first->second += range_value;
+            current += range_value;
+
+            if (current > max)
+                max = current;
+        }
+        return max;
+    }
+};
+
 int main() {
     int n;
     int m;
     cin >> n >> m;
 
-    vector<long long> arr;
-    for (size_t i = 0; i < n; i++)
-        arr.push_back(0);
-
+    ranges_t ranges;
     for (int a0 = 0; a0 < m; a0++) {
         int a; int b; int k;
         cin >> a >> b >> k;
-        for (size_t i = a-1; i < b; i++) {
-            arr[i] += k;
-        }
+        ranges.add(a, b, k);
     }
 
-    vector<long long>::iterator found = max_element(arr.begin(), arr.end());
-    cout << *found;
-
+    cout << ranges.find_max();
     return 0;
 }
